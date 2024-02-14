d863d89165e41e3e89a5083e352cc7f86ab58f07
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-02-14 15:58:44 +0100

    added functions to go to next or previous step

--- src/hooks/useMultistepForm.ts
+++ src/hooks/useMultistepForm.ts
@@ -3,9 +3,23 @@ import { ReactNode, useState } from "react";
 function useMultistepForm(steps: ReactNode[]) {
 	const [currentStep, setCurrentStep] = useState<number>(0);
 
+    function goToNextStep() {
+        if (steps.length - 1 === currentStep) return;
+
+        setCurrentStep(currentStep => currentStep + 1);
+    }
+
+    function goToPreviousStep() {
+        if (currentStep === 0) return;
+
+        setCurrentStep(currentStep => currentStep - 1);
+    }
+
 	return {
 		steps,
 		currentStep,
+        goToNextStep,
+        goToPreviousStep
 	};
 }
 
