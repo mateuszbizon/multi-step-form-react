5ae824263307ce8f7a1fc01fb2d8f9844bff16d3
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-02-17 14:00:32 +0100

    added form markup to singup content

--- src/components/SignupContent.tsx
+++ src/components/SignupContent.tsx
@@ -1,11 +1,13 @@
 function SignupContent() {
   return (
     <div className="signup-content">
-        <div className="signup-content__main-content"></div>
-        <div className="signup-content__buttons">
-            <button className="signup-content__previous-btn">Go Back</button>
-            <button className="signup-content__next-btn">Next Step</button>
-        </div>
+        <form>
+            <div className="signup-content__main-content"></div>
+            <div className="signup-content__buttons">
+                <button className="signup-content__previous-btn">Go Back</button>
+                <button className="signup-content__next-btn">Next Step</button>
+            </div>
+        </form>
     </div>
   )
 }
