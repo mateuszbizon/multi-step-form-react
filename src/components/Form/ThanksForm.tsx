e5957163abe138f28f426f6d48ea592e8d1916b9
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-02-29 17:36:43 +0100

    added thanks form styling

--- src/components/Form/ThanksForm.tsx
+++ src/components/Form/ThanksForm.tsx
@@ -1,6 +1,12 @@
+import thanksFormImg from "../../assets/images/icon-thank-you.svg";
+
 function ThanksForm() {
   return (
-    <div>ThanksForm</div>
+    <div className="thanks-form">
+      <img className="thanks-form__img" src={thanksFormImg} alt="" />
+      <h1 className="thanks-form__title">Thank you!</h1>
+      <p className="thanks-form__description">Thanks for confirming your subscription! We hope you have fun using our <br /> platform. If you ever need support, <br /> please feel free to email us at support@loremgaming.com</p>
+    </div>
   )
 }
 
